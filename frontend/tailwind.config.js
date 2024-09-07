b0f119306e2b63d3349ae25e9f0d5c8c6a5c34b4
oneju <wonju.noh.24@gmail.com>
2024-09-08 01:47:39 +0900

    design: Set Tailwind Theme #6
    tailwind.config.js 파일 수정
    figma 파일 라이브러리 기반 색상, 폰트 스타일 지정

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'red': '#EF4444',
+        'red-300': '#F87171',
+        'blue-50': '#EFF6FF',
+        'blue-400': '#60A5FA',
+        'blue-700': '#1D42D8',
+        'yellow-400': '#FBBF24',
+        'grey-50': '#F5F5F5',
+        'grey-400': '#808080',
+        'grey-500': '#A1A1AA',
+        'grey-600': '#71717A',
+      },
+      fontSize: {
+        'heading': '20px',
+        'body1': '18px',
+        'body2': '16px',
+        'body3': '14px',
+        'caption1': '12px',
+      },
+      fontWeight: {
+        'paragraph': '400',
+        'label': '600',
+        'bold':'800'
+      }
+    },
   },
   plugins: [],
   future: {
