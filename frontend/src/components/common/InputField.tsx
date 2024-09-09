da901a7468f0f4c65a6b6ac13cea7e105dc4059a
oneju <wonju.noh.24@gmail.com>
2024-09-09 16:23:06 +0900

    refactor: lint 규칙 적용

--- frontend/src/components/common/InputField.tsx
+++ frontend/src/components/common/InputField.tsx
@@ -6,9 +6,11 @@ interface InputFieldProps {
 const InputField = ({ title, error, children }: InputFieldProps) => {
   return (
     <div className="flex flex-col p-2">
-      <div className="flex justify-between items-end">
-      <label className="text-body3 font-label pb-1">{title}</label>
-      {error && <p className="text-red text-caption1 font-paragraph">* {error}</p>}
+      <div className="flex items-end justify-between">
+        <label className="pb-1 text-body3 font-label">{title}</label>
+        {error && (
+          <p className="text-caption1 font-paragraph text-red">* {error}</p>
+        )}
       </div>
       {children}
     </div>
