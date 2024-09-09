863d92d622440743ae136c20fe091b4ce32772bb
junhyejeon <junhye.jeon@gmail.com>
2024-09-09 15:02:30 +0900

    feat: 공통 Input 컴포넌트 수정 #2
    - props 에 ...rest 추가

--- frontend/src/components/common/Input.tsx
+++ frontend/src/components/common/Input.tsx
@@ -4,12 +4,13 @@ import { twMerge } from "tailwind-merge";
 type InputProps = ComponentPropsWithoutRef<"input">;
 
 const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-  const {className} = props;
+  const {className, ...rest} = props;
   return (
     <>
       <input
         className={twMerge("border", className)}
         ref={ref}
+        {...rest}
       ></input>
     </>
   )
