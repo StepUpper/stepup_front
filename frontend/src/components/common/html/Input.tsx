b7a5554c79b576a6515cc2145ef142e87ca4200f
oneju <wonju.noh.24@gmail.com>
2024-09-09 15:26:43 +0900

    design: 기본 입력 필드 생성
    label과 함께 사용되는 입력 필드 생성 [/common/inputfield.tsx](./frontend/src/common/inputfield.tsx)

---
+++ frontend/src/components/common/html/Input.tsx
@@ -0,0 +1,20 @@
+import { ComponentPropsWithoutRef, forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+type InputProps = ComponentPropsWithoutRef<"input">;
+
+const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  const {className} = props;
+  return (
+    <>
+      <input
+        className={twMerge("border", className)}
+        ref={ref}
+      ></input>
+    </>
+  )
+});
+
+Input.displayName = "Input";
+
+export default Input;
