2fc50ae8db8fcdfa941eaf39d64838cb6bab9b36
junhyejeon <junhye.jeon@gmail.com>
2024-09-10 15:08:43 +0900

    refactor: lint 규칙 적용

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,28 +1,26 @@
 import BottomButton from "@/components/common/BottomButton";
-import InputField from "@/components/common/InputField"
+import InputField from "@/components/common/InputField";
 import Input from "@/components/common/html/Input";
-import { useState } from "react"
+import { useState } from "react";
 
 const Login = () => {
-  const [ loginData, setLoginData ]  = useState({
-    email: '',
-    password: '',
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
   });
-  const [ emailError, setEmailError ] = useState(""); 
-  const [ passwordError, setPasswordError ] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
     setLoginData({
       ...loginData,
       [name]: value,
     });
 
-    if (name === 'email') setEmailError("");
-    if (name === 'password') setPasswordError("");
+    if (name === "email") setEmailError("");
+    if (name === "password") setPasswordError("");
   };
-
-  
 
   const submitHandle = (e: React.FormEvent) => {
     e.preventDefault();
@@ -37,40 +35,42 @@ const Login = () => {
         setEmailError("");
         return true;
       }
-    }
+    };
 
     let isLoginValid = true;
 
-    if (!loginData.email) { //아이디 비어있을 때
+    if (!loginData.email) {
+      //아이디 비어있을 때
       setEmailError("아이디를 입력하세요");
-      isLoginValid =false;
+      isLoginValid = false;
     } else if (validateEmail(loginData.email)) {
-      isLoginValid=true;
+      isLoginValid = true;
     }
-    
-    if (!loginData.password) { //비밀번호 비어있을 때
+
+    if (!loginData.password) {
+      //비밀번호 비어있을 때
       setPasswordError("비밀번호를 입력하세요");
-      isLoginValid=false;
+      isLoginValid = false;
     }
-    
+
     //값 확인용
     if (isLoginValid) {
-      console.log('email: ', loginData.email);
-      console.log('password: ', loginData.password)
+      console.log("email: ", loginData.email);
+      console.log("password: ", loginData.password);
     }
-  }
+  };
 
   return (
     <>
       <form className="gap-4" onSubmit={submitHandle} noValidate>
         <div className="gap-4 px-4">
           {/*아이디 입력 필드*/}
-          <InputField title="아이디" error={emailError} >
+          <InputField title="아이디" error={emailError}>
             <Input
               type="email"
               name="email"
               placeholder="이메일을 입력해주세요"
-              className="w-full h-[48px] px-4 py-[14px] rounded-[4px]"
+              className="h-[48px] w-full rounded-[4px] px-4 py-[14px]"
               value={loginData.email}
               onChange={handleInputChange}
             />
@@ -81,7 +81,7 @@ const Login = () => {
               type="password"
               name="password"
               placeholder="비밀번호를 입력해주세요"
-              className="w-full h-[48px] px-4 py-[14px] rounded-[4px]"
+              className="h-[48px] w-full rounded-[4px] px-4 py-[14px]"
               value={loginData.password}
               onChange={handleInputChange}
             />
@@ -89,11 +89,10 @@ const Login = () => {
         </div>
         <div className="mt-4 px-5">
           {/*로그인 폼 제출 버튼*/}
-          <BottomButton title="로그인"/>
+          <BottomButton title="로그인" />
         </div>
       </form>
-      
     </>
-  )
-}
-export default Login
+  );
+};
+export default Login;
