6d5ffeddabef135784c6cf0eea9370596b3a1058
Wonju <wonju.noh.24@gmail.com>
2024-09-12 16:37:17 +0900

    design: Layout 화면 전체 크기로 설정

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div className="flex h-screen flex-col items-center overflow-hidden">
-      <div className="w-full max-w-5xl">
+      <div className="size-full max-w-5xl">
         <Outlet />
       </div>
     </div>
