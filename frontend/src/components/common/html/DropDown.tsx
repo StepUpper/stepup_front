38c8f30dbd0b806fbc377b753c3986824dff0824
Seongsuk Hong <ssukh77@naver.com>
2024-09-12 13:48:26 +0900

    design: 회원가입 생년월일 선택창 view 사이즈에 맞게 수정 #21

--- frontend/src/components/common/html/DropDown.tsx
+++ frontend/src/components/common/html/DropDown.tsx
@@ -60,21 +60,21 @@ const DropDown = forwardRef<DropDownRef, DropDownProps>((props, ref) => {
       <div className="relative min-w-0 grow" ref={dropdownRef}>
         <div
           className={twMerge(
-            "flex items-center border text-body2 font-semibold",
+            "flex items-center justify-between border text-body2 font-semibold",
             className
           )}
           onClick={() => setIsOpen(!isOpen)}
         >
           <input
             className={twMerge(
-              "flex-grow cursor-pointer",
+              "w-full cursor-pointer",
               selectedOption ? "text-black" : "text-[#A1A1AA]"
             )}
             readOnly
             value={selectedOption || placeholder}
           />
           <div className="ml-2 flex size-5">
-            <img src={dropDownIcon} alt="drop down icon" className="w-full" />
+            <img src={dropDownIcon} alt="drop down icon" className="size-5" />
           </div>
         </div>
         {isOpen && (
