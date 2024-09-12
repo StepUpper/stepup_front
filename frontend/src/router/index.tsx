71a728f39fac3ac63e8674607010321ca3631888
Seongsuk Hong <ssukh77@naver.com>
2024-09-13 01:29:45 +0900

    feat: basename 추가

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -3,20 +3,25 @@ import Layout from "@components/layout/Layout";
 import NotFound from "@pages/NotFound/NotFound";
 import Chat from "@pages/Chat/Chat";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "",
+          element: <Chat />,
+        },
+      ],
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <Chat />,
-      },
-    ],
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  },
-]);
+    basename: import.meta.env.BASE_URL,
+  }
+);
 export default router;
