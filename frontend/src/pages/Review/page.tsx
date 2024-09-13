9c22d94d10510ff02e503824297eb66965588e64
Wonju <wonju.noh.24@gmail.com>
2024-09-13 12:02:09 +0900

    design: Publish /archive/review #23

--- frontend/src/pages/Review/page.tsx
+++ frontend/src/pages/Review/page.tsx
@@ -1,23 +1,25 @@
-import { plusIcon } from '@/assets/assets'
-import Header from '@/components/common/Header'
-import Button from '@/components/common/html/Button'
-import InputField from '@/components/common/InputField'
+import Header from "@common/Header";
+import ShoeRegisterInputForm from "@/components/ShoeCloset/Register/ShoeRegisterInputForm";
+import RatingComponent from "@/components/ShoeCloset/Register/RatingComponent";
+import SearchShoeButton from "@/components/ShoeCloset/Register/SearchShoeButton";
+import SubmitBottomButton from "@/components/ShoeCloset/Register/SubmitBottomButton";
 
 const page = () => {
   return (
-    <div>
-        <Header type="back">신발 등록</Header>
-        <main className="flex h-full flex-col gap-7 p-4">
-            {/* 상품 찾기 버튼 */}
-            <InputField title='신발을 선택해 주세요'>
-                <Button className="item-center w-full bg-grey-50 rounded-md p-1.5">
-                    <img src={plusIcon}/>
-                </Button>
-            </InputField>
-            {/* rating card */}
-        </main>
+    <div className="flex h-full flex-col">
+      <Header type="back">신발 등록</Header>
+      <main className="gap-7 overflow-y-scroll px-2">
+        {/* 상품 찾기 버튼 */}
+        <SearchShoeButton />
+        {/* rating card */}
+        <RatingComponent />
+        {/* input form */}
+        <ShoeRegisterInputForm />
+        {/* 등록하기 버튼 */}
+        <SubmitBottomButton/>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
