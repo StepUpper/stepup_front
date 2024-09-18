e7d254f6e48dcfd82960d97c5d182b16a540bcc5
junhyejeon <junhye.jeon@gmail.com>
2024-09-19 06:50:40 +0900

    modify: 마이쇼핑 페이지 내 컴포넌트 연결
    - MyShopping 페이지에서 탭 및 카테고리 선택 버튼 통해 상태 관리
    - 선택된 버튼에 따라 하단 div에 컴포넌트 노출되도록 변경

--- frontend/src/components/myshopping/RecentItems.tsx
+++ frontend/src/components/myshopping/RecentItems.tsx
@@ -1,4 +1,3 @@
-import Button from "@common/html/Button";
 import ProductItem from "@common/ProductItem";
 
 const RecentItems = () => {
@@ -6,14 +5,6 @@ const RecentItems = () => {
 
   return (
     <>
-      <div className="flex w-screen justify-between">
-        <Button className="h-[40px] grow border-b-2 border-black px-[16px] py-[8px] text-body2 font-bold">
-          좋아요
-        </Button>
-        <Button className="h-[40px] grow border-b border-[#E5E7EB] px-[16px] py-[8px] text-body2 font-normal text-grey-600">
-          최근 본
-        </Button>
-      </div>
       <div className="px-[16px]">
         <p className="py-[17px] text-body3 font-bold"> 총 {n} 개</p>
         <div className="mx-auto grid grid-cols-2 justify-items-center gap-[11px] py-[16px]">
