89c6a275432ad241d33ff9d29b2451a3faf6c508
Seongsuk Hong <ssukh77@naver.com>
2024-09-19 16:30:09 +0900

    refactor: 필터 바텀에서만 사용하는 label GenderCategorySelector 에서 분리 #37

--- frontend/src/components/plp/GenderCategorySelector.tsx
+++ frontend/src/components/plp/GenderCategorySelector.tsx
@@ -11,20 +11,17 @@ interface GenderCategorySelectorProps {
 const GenderCategorySelector = (props: GenderCategorySelectorProps) => {
   const { selectedGender, onClick } = props;
   return (
-    <div className="flex flex-col gap-[0.625rem]">
-      <label className="pb-1 text-[15px] font-label">성별</label>
-      <CategorySelector>
-        {GENDER_CATEGORIES.map((category) => (
-          <CategorySelector.Item
-            key={category.value}
-            isClicked={category.value === selectedGender}
-            onClick={() => onClick(category.value)}
-          >
-            {category.label}
-          </CategorySelector.Item>
-        ))}
-      </CategorySelector>
-    </div>
+    <CategorySelector>
+      {GENDER_CATEGORIES.map((category) => (
+        <CategorySelector.Item
+          key={category.value}
+          isClicked={category.value === selectedGender}
+          onClick={() => onClick(category.value)}
+        >
+          {category.label}
+        </CategorySelector.Item>
+      ))}
+    </CategorySelector>
   );
 };
 export default GenderCategorySelector;
