eac1de6f72d7be41753ba988ac35dc7d4320e068
junhyejeon <junhye.jeon@gmail.com>
2024-10-18 03:48:15 +0900

    feat: 작성일시 순으로 신발장 리스트 정렬
    - 신발장 등록시 createdAt, updatedAt 두가지 속성에 timestamp 저장
    - 문서 업데이트되면 updatedAt 속성만 수정됨
    - updatedAt 기준으로 날짜 비교하여, 리스트 정렬되도록 기능 구현
    - 기존 신발장 수정시 자동으로 최신순으로 업데이트 됨

--- frontend/src/components/shoeCloset/ShoeListComponent.tsx
+++ frontend/src/components/shoeCloset/ShoeListComponent.tsx
@@ -5,11 +5,16 @@ import { sortIcon } from "@assets/assets";
 import { useState } from "react";
 import Button from "../common/html/Button";
 
-//TODO: 신발장 db에 등록시 타임스탭프 추가하여 타임스탬프 기준으로 정렬...
+//TODO: 타임스탬프 기준으로 정렬...
 const ShoeListComponent = ({ list }: { list: IProduct[] }) => {
   const [sort, setSort] = useState(true);
 
-  const sortedList = sort ? list : list.slice().reverse();
+  const sortedList = list.slice().sort((a, b) => {
+    const aTime = a.updatedAt.seconds * 1000 + a.updatedAt.nanoseconds / 1000000;
+    const bTime = b.updatedAt.seconds * 1000 + b.updatedAt.nanoseconds / 1000000;
+
+    return sort ? bTime - aTime : aTime - bTime;
+  })
   return (
     <div className="flex flex-col gap-1.5">
       <div className="flex justify-between text-body3">
@@ -25,7 +30,7 @@ const ShoeListComponent = ({ list }: { list: IProduct[] }) => {
       <div className="grid w-full grid-cols-3 grid-rows-3 gap-1">
         <AddShoeButton />
         {sortedList.map((prod) => (
-          <ShoeComponent key={prod.shoeId} prod={prod} />
+          <ShoeComponent key={prod.closetId} prod={prod} />
         ))}
       </div>
     </div>
