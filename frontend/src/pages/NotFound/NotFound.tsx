d76ee85b38e0eefa7d64d2a24233ed3a873ff046
Seongsuk Hong <ssukh77@naver.com>
2024-11-04 00:38:31 +0900

    design: lint 적용

--- frontend/src/pages/NotFound/NotFound.tsx
+++ frontend/src/pages/NotFound/NotFound.tsx
@@ -3,18 +3,23 @@ import { shoeSearch } from "@assets/assets";
 const NotFound = () => {
   return (
     <>
-      <section className="item-center w-full h-real-screen bg-white dark:bg-zinc-800">
+      <section className="item-center h-real-screen w-full bg-white dark:bg-zinc-800">
         <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
           <div className="mx-auto max-w-screen-sm text-center">
-            <img src={shoeSearch} alt="페이지를 찾을 수 없음" className="inline-block min-w-40 md:min-w-56" />
-            <h1 className="mb-4 text-7xl font-extrabold tracking-tight text-black lg:text-9xl dark:text-white hidden">
+            <img
+              src={shoeSearch}
+              alt="페이지를 찾을 수 없음"
+              className="inline-block min-w-40 md:min-w-56"
+            />
+            <h1 className="mb-4 hidden text-7xl font-extrabold tracking-tight text-black lg:text-9xl dark:text-white">
               404
             </h1>
             <p className="mb-4 text-lg font-bold text-gray-900 md:text-2xl dark:text-white">
               죄송합니다. 현재 찾을 수 없는 페이지를 요청 하셨습니다.
             </p>
-            <p className="mb-4 text-sm font-paragraph md:text-base text-grey-500 dark:text-gray-400">
-              존재하지 않는 주소를 입력하셨거나, <br/> 요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.
+            <p className="mb-4 text-sm font-paragraph text-grey-500 md:text-base dark:text-gray-400">
+              존재하지 않는 주소를 입력하셨거나, <br /> 요청하신 페이지의 주소가
+              변경, 삭제되어 찾을 수 없습니다.
             </p>
             <a
               href="/stepup_front"
