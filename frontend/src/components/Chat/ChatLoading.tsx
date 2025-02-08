8affc9ade1a5a3b693464466e1fa36ded8e1616c
Wonju <128772605+oneju@users.noreply.github.com>
2025-02-08 21:54:06 +0900

    로그인 회원가입 수정 (#164)
    * update: Add check email valid
    
    * update: Fix login Error
    
    * fix: Remove test log

--- frontend/src/components/Chat/ChatLoading.tsx
+++ frontend/src/components/Chat/ChatLoading.tsx
@@ -2,9 +2,9 @@ const ChatLoading = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-15">
       <div className="flex space-x-6">
-        <div className="h-3 w-3 animate-pulse rounded-full bg-gray-400" />
-        <div className="animation-delay-200 h-3 w-3 animate-pulse rounded-full bg-gray-500" />
-        <div className="animation-delay-400 h-3 w-3 animate-pulse rounded-full bg-gray-600" />
+        <div className="size-3 animate-pulse rounded-full bg-gray-400" />
+        <div className="animation-delay-200 size-3 animate-pulse rounded-full bg-gray-500" />
+        <div className="animation-delay-400 size-3 animate-pulse rounded-full bg-gray-600" />
       </div>
     </div>
   );
